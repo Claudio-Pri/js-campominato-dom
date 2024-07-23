1109637839b2b46af828c43a33e29a5f988a8ef0
Claudio-Pri <priv.claudio@gmail.com>
2024-07-23 15:52:35 +0200

    html css js - casi livelli

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-console.log('js collegato');
 //===== Appunti ======
 // ` backtick
 
@@ -14,28 +13,56 @@ generateButton.addEventListener('click', () => {
     console.log('Difficoltà: ', diffSelector.value);
     // gridcontainer.innerhtml=''; per resettare la board 
     gridContainer.innerHTML = ('');
-    // Livelli
-    let cellsNumber = 100;
-    if (diffSelector.value == '2') {
-        cellsNumber = 81;
-    }
-    else if (diffSelector.value == '3') {
-        cellsNumber = 49;
-    }
+    // livelli
+    let cellsNumber = parseInt(diffSelector.value);
+
+    // switch (diffSelector.value) {
+    //     case '2':
+    //         cellsNumber = 81;
+    //         break;
+    //     case '3':
+    //         cellsNumber = 49;
+    //         break;
+    //     default:
+    //         cellsNumber = 100;
+    //         break;
+    // };
+
+    // if (diffSelector.value == '2') {
+    //     cellsNumber = 81;
+    // }
+    // else if (diffSelector.value == '3') {
+    //     cellsNumber = 49;
+    // }
     // genero l'elemento
     for (let i = 0; i < cellsNumber; i++) {
         let newCell = document.createElement('div');
         newCell.innerHTML = (i + 1);
         // ridimensionamento celle in base al livello
-        if (cellsNumber == 100) {
-            newCell.classList.add('easy');
-        }
-        else if (cellsNumber == 81) {
-            newCell.classList.add('medium');
-        }
-        else if (cellsNumber == 49) {
-            newCell.classList.add('hard');
-        }
+        // opzione 3 (modificate classi css)
+        newCell.classList.add('cell-' + cellsNumber);
+        // opzione 1:
+        // if (cellsNumber == 100) {
+        //     newCell.classList.add('easy');
+        // }
+        // else if (cellsNumber == 81) {
+        //     newCell.classList.add('medium');
+        // }
+        // else if (cellsNumber == 49) {
+        //     newCell.classList.add('hard');
+        // }
+        // opzione 2:
+        // switch (cellsNumber) {
+        //     case 81:
+        //         newCell.classList.add('medium');
+        //         break;
+        //     case 49:
+        //         newCell.classList.add('hard');
+        //         break;
+        //     default:
+        //         newCell.classList.add('easy');
+        //         break;
+        // };
         //aggiungo event listener sulla cella
         newCell.addEventListener('click', function () {
             //perchè This funziona con - function () - ma non con - () => -?
